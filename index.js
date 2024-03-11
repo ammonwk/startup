205d84be7e705320b3594bf453cd3e232844b709
Ammon Kunzler <ammonwk@gmail.com>
2024-03-11 14:34:00 -0600

    Fixed index.js port

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
 // Middleware - Static file hosting
 app.use(express.static('public'));
 
-// Listening to a network port
-const port = 8080;
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
 app.listen(port, function () {
     console.log(`Listening on port ${port}`);
 });
