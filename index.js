3fa68cb83945ea6925de966c8d17cb5219bdbc44
Ammon Kunzler <ammonwk@gmail.com>
2024-03-22 23:42:13 -0600

    Added authentication to user creation

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('rm_data');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const cookieParser = require('cookie-parser');
 
 // Connect to the database
 (async function testConnection() {
@@ -16,6 +19,21 @@ const db = client.db('rm_data');
     console.log(`Unable to connect to database with ${url} because ${ex.message}`);
     process.exit(1);
 });
+
+async function createUser(username, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        username: username,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    await db.collection('users').insertOne(user);
+
+    return user;
+}
+
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
@@ -29,10 +47,16 @@ var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
 apiRouter.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await db.collection('users').findOne({ username: username, password: password });
+    const user = await db.collection('user').findOne({ email: req.body.email });
     if (user) {
-        res.send({ username: user.username });
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.cookie("token", user.token, {
+                secure: true,
+                httpOnly: true,
+                sameSite: 'strict',
+            });
+            res.send({ id: user._id });
+        }
     } else {
         res.status(401).send({ msg: 'Invalid username or password' });
         console.log('Invalid username or password');
@@ -46,8 +70,15 @@ apiRouter.post('/signup', async (req, res) => {
         res.status(409).send({ msg: 'Username taken' });
         console.log('Username already exists');
     } else {
-        await db.collection('users').insertOne({ username: username, password: password });
-        res.send({ username: username });
+        const user = await createUser(req.body.username, req.body.password);
+        res.cookie("token", user.token, {
+            secure: true,
+            httpOnly: true,
+            sameSite: 'strict',
+        });
+        res.send({
+            id: user._id,
+        });
     }
 });
 
@@ -67,6 +98,20 @@ apiRouter.post('/events', (req, res) => {
 app.use((err, req, res, next) => {
     console.error(err.stack); // Log the error stack for debugging
     res.status(500).send('Something broke!'); // Send a generic error message
+});
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+    authToken = req.cookies[authCookieName];
+    const user = await db.collection('users').findOne({ token: token });
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
 });
 
 
