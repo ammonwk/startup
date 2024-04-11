09d8fbe9e3433e7e2480d9c1a9b6bc369522e70b
Ammon Kunzler <ammonwk@gmail.com>
2024-04-10 23:05:46 -0600

    Added router

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login';
+import { Planner } from './planner';
+import { Home } from './home';
+import { Signup } from './signup';
 
 export default function App() {
     return (
         <>
-            <header>
-                <nav className="navbar navbar-expand-sm navbar-light bg-white border-bottom mx-3 my-2">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="index.html">RM Planner</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <a className="nav-link" href="index.html">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="planner.html">Planner</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link me-2" href="login.html">Login</a>
-                                </li>
-                            </ul>
-                            <a className="btn btn-outline-primary" href="signup.html">Sign up</a>
+            <BrowserRouter>
+                <header>
+                    <nav className="navbar navbar-expand-sm navbar-light bg-white border-bottom mx-3 my-2">
+                        <div className="container-fluid">
+                            <NavLink className="navbar-brand" to="/">RM Planner</NavLink>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/">Home</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to="/planner">Planner</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link me-2" to="/login">Login</NavLink>
+                                    </li>
+                                </ul>
+                                <NavLink className="btn btn-outline-primary" to="/signup">Sign up</NavLink>
+                            </div>
                         </div>
-                    </div>
-                </nav>
-            </header>
-            <main>App components go here</main>
+                    </nav>
+                </header>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/planner" element={<Planner />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+            </BrowserRouter>
             <footer className="mt-auto bg-light d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <nav aria-label="Footer legal">
                     <nav aria-label="Footer legal">
---
+++ src/planner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Planner() {
+    return (
+        <main className='container-fluid bg-secondary text-center'>
+            <div>planner displayed here</div>
+        </main>
+    );
+}
