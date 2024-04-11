c562247e58dff1d923c29ac31ea7c3924566bd06
Ammon Kunzler <ammonwk@gmail.com>
2024-04-11 00:43:59 -0600

    Ported Home page

--- src/home.jsx
+++ src/home.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 export function Home() {
     return (
         <main className='container-fluid bg-secondary text-center'>
-            <div>home displayed here</div>
+            <div className="bg-image">
+                <div className="col-md-5 p-lg-5 mx-auto my-5 text-center text-white">
+                    <h1 className="display-4 font-weight-normal" id="pad-top">RM Planner - Home</h1>
+                    <p className="lead font-weight-normal">Welcome, to the Areabook you've been missing so long.</p>
+                    <a className="btn btn-outline-light" id="mb-6" href="signup.html">Get Started</a>
+                </div>
+            </div>
         </main>
     );
 }
